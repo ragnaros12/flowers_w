36a3470ea6ee1a01cdea1d569ceed9a0826ac80f
artem <khlopovartem432109@gmail.com>
2023-04-10 16:04:34 +0300

    Первый урок

---
+++ Asd/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Asd.Models;
+
+namespace Asd.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        List<Flower> flowers = new List<Flower> {
+            new()
+            {
+                Description = "Цветок Цветок Цветок Цветок Цветок Цветок Цветок Цветок Цветок",
+                Height = 100,
+                Id = 1,
+                Image = "images/pions.jpeg",
+                Name = "Пионы",
+                Price = 100
+            },
+            new()
+            {
+                Description = "Цветок Цветок Цветок Цветок Цветок Цветок Цветок Цветок Цветок",
+                Height = 100,
+                Id = 1,
+                Image = "images/pions.jpeg",
+                Name = "Пионы",
+                Price = 100
+            },
+            new()
+            {
+                Description = "Цветок Цветок Цветок Цветок Цветок Цветок Цветок Цветок Цветок",
+                Height = 100,
+                Id = 1,
+                Image = "images/pions.jpeg",
+                Name = "Пионы",
+                Price = 100
+            },
+            new()
+            {
+                Description = "Цветок Цветок Цветок Цветок Цветок Цветок Цветок Цветок Цветок",
+                Height = 100,
+                Id = 1,
+                Image = "images/pions.jpeg",
+                Name = "Пионы",
+                Price = 100
+            },
+            new()
+            {
+                Description = "Цветок Цветок Цветок Цветок Цветок Цветок Цветок Цветок Цветок",
+                Height = 100,
+                Id = 1,
+                Image = "images/pions.jpeg",
+                Name = "Пионы",
+                Price = 100
+            }
+        };
+        return View(flowers);
+    }
+}
---
+++ Asd/Models/Flower.cs
@@ -0,0 +1,11 @@
+namespace Asd.Models;
+
+public class Flower
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Height { get; set; }
+    public string Image { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+}
